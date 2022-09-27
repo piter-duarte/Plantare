8a1aafa9b0eaf4ae99dba70d10c1715d6b86fbfe
piter-duarte <piter__duarte66@hotmail.com>
2022-09-27 00:17:00 -0300

    Implemented basic provider search using cookies

--- index.php
+++ index.php
@@ -4,19 +4,24 @@
     $con= new Models\ModelConect;
     $con->conectDB();
 ?>
+<header class="mdc-top-app-bar">
+  <div class="mdc-top-app-bar__row">
+    <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+      <span class="mdc-top-app-bar__title"><img class="img-logo" src="./lib/img/logo2.png" height="50px"></span>
+    </section>
+    <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+    <button class="mdc-button mdc-button--raised cadastrar">
+        <span class="mdc-button__label"><a href="./views/cadastro.php">Cadastrar</a></span>
+    </button>
+    <button class="mdc-button mdc-button--raised logar">
+        <span class="mdc-button__label"><a href="./views/login.php">Logar</a></span>
+    </button>
+    </section>
+  </div>
+</header>
 
-<div class="container-top">
-    <img class="img-logo" src="./lib/img/logo2.png">
-    
-    <button class="container-top btn" id="tpCadastrar"><a href="./views/cadastro.php">Cadastrar</a></button>
-    <button class="container-top btn" id="tpLogar"><a href="./views/login.php">Logar</a></button>
-</div>
-
-
-
-<div class="container-mid">
-    <div class="container-mid-espaco1"></div>
-    <div class="container-mid-espaco2"></div>
+<div class="index-img-container">
+        <h1>imagem</h1>
 </div>
 
 <div class="container-bottom">
--- views/user/add.php
+++ views/user/add.php
@@ -6,6 +6,10 @@
     $objBDD            = new \Classes\ClassBDD();
     $resultadoServices = $objBDD->getServices();
     $resultadoProvider = $objBDD->getProviders(1);
+    if(isset($_COOKIE["hora"]) == false)
+    {
+        setcookie('hora','1', 0 ); 
+    }
 ?>
 
 <form name="formAdd" id="formAdd" method="post"action="<?php echo DIRPAGE.'/controllers/ControllerAddEvent.php'; ?>">
@@ -18,7 +22,7 @@
          {
             $idServico = $linha['id'];
             $servico   = $linha['nomeS'];
-            if($idServico == $_GET['id'])
+            if($idServico == $_COOKIE["id"])
             {
                 echo "<option value='$idServico' selected>$servico</option>";
             }
@@ -32,16 +36,37 @@
     Preço: <input type="text" name="description" id="description" value=""> <br>
     Quanto tempo deseja de atendimento: 
     <select name="horasAtendimento" id="horasAtendimento">
-        <option value="1">1h</option>
-        <option value="2">2h</option>
-        <option value="3">3h</option>
+        <?php
+        switch ($_COOKIE["hora"]) {
+            case 1:
+                echo "<option value='1' selected>1h</option>";
+                echo "<option value='2'>2h</option>";
+                echo "<option value='3'>3h</option>";
+                break;
+            case 2:
+                echo "<option value='1'>1h</option>";
+                echo "<option value='2' selected>2h</option>";
+                echo "<option value='3'>3h</option>";
+                break;
+            case 3:
+                echo "<option value='1'>1h</option>";
+                echo "<option value='2'>2h</option>";
+                echo "<option value='3' selected>3h</option>";
+            break;
+            default:
+                echo "<option value='1'>1h</option>";
+                echo "<option value='2'>2h</option>";
+                echo "<option value='3'>3h</option>";
+                break;
+        }
+        ?>
     </select><br>
     Profissional Responsável: <select name="provider_key">
     <?php
-            if(isset($_GET['id']))
+            if(isset($_COOKIE["id"]))
             {
 
-                $resultadoProvider = $objBDD->getProviders($_GET['id']);
+                $resultadoProvider = $objBDD->getProviders($_COOKIE["id"]);
                 foreach($resultadoProvider as $linha)
                 {
                    $provider_nome  = $linha['nome'];
