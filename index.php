d093cb8c433db2a63f0bb8a7cc5d2f144c3f2d02
piter-duarte <piter__duarte66@hotmail.com>
2022-11-01 00:29:42 -0300

    Beginning of the implementation of the v2 layout of figma

--- index.php
+++ index.php
@@ -4,28 +4,97 @@
     $con= new Models\ModelConect;
     $con->conectDB();
 ?>
-<header class="mdc-top-app-bar">
-  <div class="mdc-top-app-bar__row">
-    <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-      <span class="mdc-top-app-bar__title"><img class="img-logo" src="./lib/img/logo2.png" height="50px"></span>
-    </section>
-    <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
-    <button class="mdc-button mdc-button--raised cadastrar">
-        <span class="mdc-button__label"><a href="./views/cadastro.php">Cadastrar</a></span>
-    </button>
-    <button class="mdc-button mdc-button--raised logar">
-        <span class="mdc-button__label"><a href="./views/login.php">Logar</a></span>
-    </button>
-    </section>
+<div class="container">
+
+  <header>
+        <div class="logo">
+          <img class="img-logo" src="./lib/img/LogoAlterada.png">
+          <h6>Plantare</h6>
+        </div>
+        <div class="btns">
+          <a class="button-main" href="./views/cadastro.php">
+            <h6>CONECTE-SE</h6>
+          </a>
+          <a class="button-secondary" href="./views/login.php">
+            <h6>LOGIN</h6>
+          </a>
+        </div>       
+  </header>
+
+  <main>
+    <h2 class="hero">Facilitando a sua conexão com a natureza</h2>
+    <div class="slider">
+          <div class="slides">
+              <!--Radio Buttons-->
+              <input type="radio" name="radio-btn" id="radio1">
+              <input type="radio" name="radio-btn" id="radio2" checked>
+              <input type="radio" name="radio-btn" id="radio3">
+              <!--fim Radio Buttons-->
+
+              <!--Slide images-->
+              <div class="slide first">
+                <img src="./lib/img/image1.png" alt="imagem 1">
+              </div>
+
+              <div class="slide">
+                <img src="./lib/img/image2.png" alt="imagem 2">
+              </div>
+
+              <div class="slide">
+                <img src="./lib/img/image3.png" alt="imagem 3">
+              </div>
+              <!--fim Slide images-->
+
+              <!--Navigation auto-->
+              <div class="navigation-auto">
+                <div class="auto-btn1"></div>
+                <div class="auto-btn2"></div>
+                <div class="auto-btn3"></div>
+              </div>
+          </div>
+
+          <div class="manual-navigation">
+            <label for="radio1" class="manual-btn"></label>
+            <label for="radio2" class="manual-btn"></label>
+            <label for="radio3" class="manual-btn"></label>
+          </div>
+      </div>
+  
+    <div class="qsomos">
+      <h3>QUEM<br>SOMOS</h3>
+      <p>Somos uma plataforma focada no ramo de jardinagem, promovendo um espaço para a busca e contratação de profissionais do ramo</p>
+    </div>
+    
+    <div class="nservicos">
+      <h2>NOSSOS<br>SERVIÇOS</h2>
+      <img class="nservicosimg" src="./lib/img/nservicosimg.png" alt="nservicosimg">
+      <div class="servicos">
+        <div class="servicosgrid" id="servico1">
+            <img src="./lib/img/Grama.png" alt="algo">
+            <p>Cortar Grama</p>
+        </div>
+        <div class="servicosgrid" id="servico2">
+        <img src="./lib/img/Poda.png" alt="algo">
+            <p>Realizar Poda</p>
+        </div>
+        <div class="servicosgrid" id="servico3">
+            <img src="./lib/img/Fertiliza.png" alt="algo">
+            <p>Aplicar Fertilizante</p>
+        </div>
+        <div class="servicosgrid" id="servico4">
+            <img src="./lib/img/Pesticida.png" alt="algo">
+            <p>Aplicar Pesticida</p>
+        </div>
+      </div>
+    </div>
+
+  <div class="nprofissionais">
+
   </div>
-</header>
 
-<div class="index-img-container">
-        <h1>imagem</h1>
+    </main>
+
 </div>
 
-<div class="container-bottom">
-    <div class="bottom-footer"></div>
-</div>
 
 <?php include(DIRREQ."/lib/html/footer.php"); ?>
--- lib/html/header.php
+++ lib/html/header.php
@@ -18,7 +18,5 @@
     <!--JQuery-->
     <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
 
-    <!--Starr-->
-    <link rel="stylesheet" href="<?php echo DIRPAGE.'/lib/css/starrr.css';?>">
 </head>
 <body>
