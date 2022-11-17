b2ab8bb90ece1b39803eeb743ec3be09ff427daa
BrunoFEL89 <brunofelipe.ellerluz@gmail.com>
2022-11-16 23:58:45 -0300

    criacaodapage

--- lib/html/header.php
+++ lib/html/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Calendário Agenda com PHP</title>
+    <title>Plantare</title>
     
     <!--FullCalendar-->
     <link rel="stylesheet" href="<?php echo DIRPAGE.'/lib/js/FullCalendar/main.min.css';?>">
--- realizarOrcamento.php
+++ realizarOrcamento.php
@@ -15,18 +15,97 @@
             chamarNavbar('Pagina em Desenvolvimento do Bruno');
         ?>
         <div class="conteudo realizarOrcamento">
-            <div class="space80">     
-                    <div class="containerform">
-                        <input type="text">
-                        <input type="text">
-                        <input type="text">
-                        <input type="text">
-                        <input type="text">
-                        <input type="button">
+            <div class="space80">
+                <form class="containerform" id="realizarOrcamento" action="">
+                    <div class="formGroup flex flex-col my-4">
+                        <label for="date">Data</label>
+                        <div class="flex items-center">
+
+                            <input id="date" type="date" class="w-full border-b-2 border-black outline-none py-2"
+                                required>
+                            <span class="error-icon hidden -ml-6 text-red-700">
+                                <i class="fa-solid fa-circle-exclamation"></i>
+                            </span>
+                            <span class="success-icon hidden -ml-6 text-green-700">
+                                <i class="fa-solid fa-circle-check"></i>
+                            </span>
+                        </div>
+                        <div class="error text-red-700 py-2"></div>
                     </div>
+
+                    <div class="formGroup flex flex-col my-4">
+                        <label for="date">Hora Inicial</label>
+                        <div class="flex items-center">
+
+                            <input id="time" type="time" class="w-full border-b-2 border-black outline-none py-2"
+                                required>
+                            <span class="error-icon hidden -ml-6 text-red-700">
+                                <i class="fa-solid fa-circle-exclamation"></i>
+                            </span>
+                            <span class="success-icon hidden -ml-6 text-green-700">
+                                <i class="fa-solid fa-circle-check"></i>
+                            </span>
+                        </div>
+                        <div class="error text-red-700 py-2"></div>
+                    </div>
+
+                    <div class="formGroup flex flex-col my-4">
+                        <label for="time">Tempo Atendimento</label>
+                        <div class="flex items-center">
+                            <select name="horasAtendimento" id="horasAtendimento"
+                                class="w-full border-b-2 border-black outline-none py-2">
+                                <!-- Options do Select-->
+                            </select>
+                            <span class="error-icon hidden -ml-6 text-red-700">
+                                <i class="fa-solid fa-circle-exclamation"></i>
+                            </span>
+                            <span class="success-icon hidden -ml-6 text-green-700">
+                                <i class="fa-solid fa-circle-check"></i>
+                            </span>
+                        </div>
+                        <div class="error text-red-700 py-2"></div>
+                    </div>
+
+                    <div class="formGroup flex flex-col my-4">
+                        <label for="title">Serviço</label>
+                        <div class="flex items-center">
+
+                            <select name="title" id="title" class="w-full border-b-2 border-black outline-none py-2">
+                                <!-- Options do Select-->
+                            </select>
+                            <span class="error-icon hidden -ml-6 text-red-700">
+                                <i class="fa-solid fa-circle-exclamation"></i>
+                            </span>
+                            <span class="success-icon hidden -ml-6 text-green-700">
+                                <i class="fa-solid fa-circle-check"></i>
+                            </span>
+                        </div>
+                        <div class="error text-red-700 py-2"></div>
+                    </div>
+
+                    <div class="formGroup flex flex-col my-4">
+                        <label for="provider_key">Profissional</label>
+                        <div class="flex items-center">
+                            <select name="provider_key" id="provider_key"
+                                class="w-full border-b-2 border-black outline-none py-2">
+                                <!-- Options do Select-->
+                            </select>
+                            <span class="error-icon hidden -ml-6 text-red-700">
+                                <i class="fa-solid fa-circle-exclamation"></i>
+                            </span>
+                            <span class="success-icon hidden -ml-6 text-green-700">
+                                <i class="fa-solid fa-circle-check"></i>
+                            </span>
+                        </div>
+                        <div class="error text-red-700 py-2"></div>
+                    </div>
+                    
+                    <input class="btxg" type="button" value="Solicitar Orçamento">
+                </form>
             </div>
         </div>
-    </main>
+</div>
+</main>
 
 </div>
 
