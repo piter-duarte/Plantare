c462f3e4c57353b4744bd2d05627b82a10985eab
BrunoFEL89 <brunofelipe.ellerluz@gmail.com>
2022-11-17 10:28:14 -0300

    criar avaliarProfissional

--- views/user/avaliarProfissional.php
+++ views/user/avaliarProfissional.php
@@ -14,7 +14,44 @@
         <?php
             chamarNavbar($_SESSION["nome"]);
         ?>
-        <div class="conteudo"></div>
+        <div class="conteudo avaliarProfissional">
+            <div class="contendmiddle">
+
+                <div class="titleAvaliar">
+                    <h3>Avaliar prestador de Serviço</h3>
+                </div>
+
+                <div class="fotoUser">
+                    <!-- <img id="img-user" src="'.DIRPAGE.'./../lib/img/userImage.png" alt=""> -->
+                    <img id="img-user" src="../../lib/img/userImage.png" alt="">
+                </div>
+
+                <div class="avaliar">
+                    <img id="img-user" src="../../lib/img/userImage.png" alt="">
+                    <h5>'.$usuario.'</h5>
+                    <!-- <div class="stars">
+                        <i class="fa-solid fa-star fa-sm"></i>
+                        <i class="fa-solid fa-star fa-sm"></i>
+                        <i class="fa-solid fa-star fa-sm"></i>
+                        <i class="fa-solid fa-star fa-sm"></i>
+                        <i class="fa-solid fa-star fa-sm"></i>
+                    </div>-->
+                </div>
+                <div class="btnAvaliar">
+                <input class="btg" type="button" value="Avaliar">
+                </div>
+            </div>
+            <!-- 
+
+            display grid
+            
+            grid-template-rows: 1fr 1fr 1fr
+            grid-template-collumns: 10% 5% 1fr
+            grid-row-start: 1
+            grid-row-end: 3
+            -->
+
+        </div>
     </main>
 
 </div>
