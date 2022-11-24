a3dc8ca53697772d94d35207a57762f96cd3dc84
BrunoFEL89 <brunofelipe.ellerluz@gmail.com>
2022-11-24 19:42:41 -0300

    statusPedidos

--- views/user/statusPedidos.php
+++ views/user/statusPedidos.php
@@ -15,7 +15,40 @@
         <?php
             chamarNavbar($_SESSION["nome"]);
         ?>
-        <div class="conteudo"></div>
+                <div class="conteudo StatusPedido">
+            <div class="contendmiddle">
+                <form name="formAdd" id="formAdd" method="post">
+                    <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+                    <input type="hidden" name="provider_key" value="<?php echo $_GET['provider_key']; ?>">
+
+                    <div class="titleStatus">
+                        <h3>Status do Serviço</h3>
+                    </div>
+
+                    <div class="fotoUser">
+                        <img id="img-user" src="../../lib/img/userImage.png" alt="">
+                    </div>
+
+                    <div class="avaliar">
+                        <h5>Usuário</h5>
+                        <select name="avaliacao">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+
+                    <div class="btnAvaliar">
+                        <input class="btm bts" type="submit" value="Cancelar"
+                            formaction="<?php echo DIRPAGE.'/views/user/novoPedido.php'; ?>">
+                        <input class="btm" type="submit" value="Avaliar"
+                            formaction="<?php echo DIRPAGE.'/controllers/ControllerUpdateRating.php'; ?>">
+                    </div>
+                </form>
+            </div>
+        </div>
     </main>
 
 </div>
