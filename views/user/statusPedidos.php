33440b4400dddee14e5361091cc05a555c85d399
piter-duarte <piter__duarte66@hotmail.com>
2022-12-05 19:15:53 -0300

    statusPedido update

--- views/user/statusPedidos.php
+++ views/user/statusPedidos.php
@@ -3,7 +3,6 @@
     include(DIRREQ."/lib/html/header.php");
     require_once DIRREQ."/lib/includes/valida-acesso.inc.php";
     include(DIRREQ."/lib/includes/funcoes.php");
-
     $usuario = unserialize($_SESSION['usuario']);
 ?>
 
@@ -16,7 +15,39 @@
         <?php
        chamarNavbar($usuario);
         ?>
-        <div class="conteudo"></div>
+        <div class="conteudo StatusPedido">
+            <div class="contendmiddle">
+
+                <div class="infoStatus">
+
+                    <div class="titleStatus">
+                        <h3>Status do Pedido</h3>
+                    </div>
+
+                    <div class="status">
+                        <h4>Serviço: </h4> 
+                        <h4>Solicitando:</h4>
+                        <h4>Prestador:</h4>
+                        <h4>Data:</h4>
+                        <h4>Horário Inicio:</h4>
+                        <h4>Horário Fim:</h4>
+                        <h4>Status:</h4>
+                    </div>
+
+                    <form class="btnStatus" name="formStatusPedido" id="formStatusPedido" method="post">
+                        <div>
+                            <input class="btm" type="submit" value="Confirmar"
+                                formaction="<?php echo DIRPAGE.'/controllers/aprovarPedidoAceitarController.php'; ?>">
+                            <input class="btm bts" type="submit" value="Cancelar"
+                                formaction="<?php echo DIRPAGE.'/views/user/aprovarPedidoCancelarController.php.php'; ?>">
+                        </div>
+                        <input class="btm bts" type="submit" value="Voltar"
+                            formaction="<?php echo DIRPAGE.'/views/user/meuCalendario.php'; ?>">
+                    </form>
+
+                </div>
+            </div>
+        </div>
     </main>
 
 </div>
