f9b327e81ef69beecc86a3bd6ecb839a9ecf5e6a
BrunoFEL89 <brunofelipe.ellerluz@gmail.com>
2022-12-04 21:14:18 -0300

    criação layout

--- views/user/statusPedidos.php
+++ views/user/statusPedidos.php
@@ -18,32 +18,34 @@
         <div class="conteudo StatusPedido">
             <div class="contendmiddle">
 
-                <form name="formStatusPedido" id="formStatusPedido" method="post">
-
+                <div class="infoStatus">
 
                     <div class="titleStatus">
                         <h3>Status do Pedido</h3>
                     </div>
 
                     <div class="status">
-                        <h5 >Serviço:</h5>
-                        <h5 >Solicitando:</h5>
-                        <h5 >Prestador:</h5>
-                        <h5 >Data:</h5>
-                        <h5 >Horário Inicio:</h5>
-                        <h5 >Horário Fim:</h5>
-                        <h5 >Status:</h5>
+                        <h4>Serviço: </h4>
+                        <h4>Solicitando:</h4>
+                        <h4>Prestador:</h4>
+                        <h4>Data:</h4>
+                        <h4>Horário Inicio:</h4>
+                        <h4>Horário Fim:</h4>
+                        <h4>Status:</h4>
                     </div>
 
-                    <div class="btnStatus">
-                        <input class="btxg" type="submit" value="Cancelar Pedido"
-                            formaction="<?php echo DIRPAGE.'/views/user/novoPedido.php'; ?>">
-                        <input class="btm bts" type="submit" value="Confirmar"
-                            formaction="<?php echo DIRPAGE.'/controllers/ControllerUpdateRating.php'; ?>">
+                    <form class="btnStatus" name="formStatusPedido" id="formStatusPedido" method="post">
+                        <div>
+                            <input class="btm" type="submit" value="Confirmar"
+                                formaction="<?php echo DIRPAGE.'/controllers/ControllerUpdateRating.php'; ?>">
+                            <input class="btm bts" type="submit" value="Cancelar"
+                                formaction="<?php echo DIRPAGE.'/views/user/novoPedido.php'; ?>">
+                        </div>
                         <input class="btm bts" type="submit" value="Voltar"
-                            formaction="<?php echo DIRPAGE.'/views/user/meuPerfil.php'; ?>">
-                    </div>
-                </form>
+                            formaction="<?php echo DIRPAGE.'/views/user/meuCalendario.php'; ?>">
+                    </form>
+
+                </div>
             </div>
         </div>
     </main>
